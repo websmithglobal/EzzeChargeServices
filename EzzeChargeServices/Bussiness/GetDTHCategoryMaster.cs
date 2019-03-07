9e3af26fd035e162da0c93f039831022e983e0fd
Kalpesh <kg@websmithsolution.com>
2019-03-07 17:33:08 +0530

    Create two api for BoxTypeMaster And CategoryMaster

---
+++ EzzeChargeServices/Bussiness/GetDTHCategoryMaster.cs
@@ -0,0 +1,26 @@
+﻿using EzzeChargeServices._MyConnection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace EzzeChargeServices.Bussiness
+{
+    public class GetDTHCategoryMaster
+    {
+        public List<DataClass.DataCategoryMaster.CategoryMaster> GetDTHCategory()
+        {
+            List<DataClass.DataCategoryMaster.CategoryMaster> BalList = new List<DataClass.DataCategoryMaster.CategoryMaster>();
+            DataTable dt = SqlHelper.GetDataUsingQuery("SELECT categoryid, categoryname, categorystatus FROM CategoryMaster WHERE categorystatus=1");
+            if (dt.Rows.Count > 0)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    BalList.Add(new DataClass.DataCategoryMaster.CategoryMaster(Int64.Parse(row["categoryid"].ToString()), row["categoryname"].ToString(), bool.Parse(row["categorystatus"].ToString())));
+                }
+            }
+            return BalList;
+        }
+    }
+}
---
+++ EzzeChargeServices/DataClass/DataCategoryMaster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EzzeChargeServices.DataClass
+{
+    public class DataCategoryMaster
+    {
+        public class CategoryMaster
+        {
+            public Int64 categoryid { get; set; }
+            public string categoryname { get; set; }
+            public bool categorystatus { get; set; }
+
+            public CategoryMaster(Int64 _categoryid, string _categoryname, bool _categorystatus)
+            {
+                this.categoryid = _categoryid;
+                this.categoryname = _categoryname;
+                this.categorystatus = _categorystatus;
+            }
+        }
+    }
+}
